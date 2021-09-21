1c117ca270df6c7b9a43949dcd80af64e7aba9f9
wang.haoyu <228233464@qq.com>
2021-09-21 17:12:01 +0800

    feat: http封装

---
+++ src/utils/http.ts
@@ -0,0 +1,45 @@
+import { logout } from "auth-provider";
+
+interface HttpParams {
+  data?: any;
+  token?: string;
+  config?: RequestInit;
+}
+
+const baseUrl = process.env.REACT_APP_API_URL;
+
+export const http = async (
+  endPoint: string,
+  { data, token, config }: HttpParams
+) => {
+  const requestData = {
+    method: "GET",
+    headers: {
+      Authorization: token ? "Bearer" + token : "",
+      "Content-Type": data ? "application/json" : "",
+    },
+    ...config,
+  };
+
+  if (requestData.method.toUpperCase() === "GET") {
+    endPoint += new URLSearchParams(data).toString();
+  } else {
+    requestData.body = JSON.stringify(data || {});
+  }
+
+  return window.fetch(`${baseUrl}${endPoint}`, requestData).then(
+    async (res) => {
+      if (res.status === 401) {
+        await logout();
+        window.location.reload();
+        return Promise.reject({ message: "请重新登录" });
+      }
+      if (res.ok) {
+        return res;
+      } else {
+        return Promise.reject(res);
+      }
+    },
+    (error) => Promise.reject(error)
+  );
+};
